4c72715b2c4526bf08fa2d15b5f3fb12ab81eb56
Sergio Pina <sepi_147@hotmail.com>
2024-08-16 17:50:11 -0500

    Init commit

---
+++ src/main/java/com/challenge/spotify_challenge/SpotifyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.spotify_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/spotify_challenge/SpotifyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.spotify_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
